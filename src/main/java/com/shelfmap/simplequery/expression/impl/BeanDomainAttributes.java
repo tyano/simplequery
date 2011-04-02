d118ab15c66f552810a5ce152512825bca20a251
Tsutomu YANO <t_yano@me.com>
2011-04-02 18:52:25 +0900

    handled new @FlatAttribute, but some TODO remains (not finished)

--- src/main/java/com/shelfmap/simplequery/expression/impl/BeanDomainAttributes.java
+++ src/main/java/com/shelfmap/simplequery/expression/impl/BeanDomainAttributes.java
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.shelfmap.simplequery.expression.impl;
 
 import static com.shelfmap.simplequery.util.Assertion.isNotNull;
 import com.shelfmap.simplequery.Domain;
+import com.shelfmap.simplequery.FlatAttribute;
 import com.shelfmap.simplequery.FloatAttribute;
 import com.shelfmap.simplequery.IntAttribute;
 import com.shelfmap.simplequery.LongAttribute;
@@ -42,83 +42,127 @@ import java.util.Map;
  * @author Tsutomu YANO
  */
 public class BeanDomainAttributes implements DomainAttributes {
-    private final Map<String,DomainAttribute<?>> attributeMap = new LinkedHashMap<String, DomainAttribute<?>>();
+
+    private final Map<String, DomainAttribute<?>> attributeMap = new LinkedHashMap<String, DomainAttribute<?>>();
     private final Class<?> domainClass;
-    private final Map<String,Method> writeMethodMap = new HashMap<String,Method>();
-    
+    private final Map<String, Method> writeMethodMap = new HashMap<String, Method>();
+
     public BeanDomainAttributes(Class<?> domainClass) {
         isNotNull("domainClass", domainClass);
-        if( !domainClass.isAnnotationPresent(Domain.class)) {
+        if (!domainClass.isAnnotationPresent(Domain.class)) {
             throw new IllegalArgumentException("domainClass must have a @Domain annotation.");
         }
-        
+
         this.domainClass = domainClass;
         try {
             BeanInfo info = Introspector.getBeanInfo(domainClass);
             PropertyDescriptor[] descriptors = info.getPropertyDescriptors();
-            for(PropertyDescriptor descriptor : descriptors) {
+            for (PropertyDescriptor descriptor : descriptors) {
                 Class<?> type = descriptor.getPropertyType();
                 String propertyName = descriptor.getName();
                 Method getter = descriptor.getReadMethod();
                 Method setter = descriptor.getWriteMethod();
-                DomainAttribute<?> attribute = createAttribute(propertyName, type, getter);
-                attributeMap.put(attribute.getName(), attribute);
-                writeMethodMap.put(attribute.getName(), setter);
-            }   
+                
+                if(getter.isAnnotationPresent(FlatAttribute.class)) {
+                    buildFlatAttribute(type);
+                } else {
+                    DomainAttribute<?> attribute = createAttribute(propertyName, type, getter);
+                    attributeMap.put(attribute.getName(), attribute);
+                    writeMethodMap.put(attribute.getName(), setter);
+                }
+            }
         } catch (IntrospectionException ex) {
             throw new IllegalStateException("Can not introspect a class object.", ex);
         }
     }
-    
+
     @SuppressWarnings("unchecked")
     private <C> DomainAttribute<?> createAttribute(String propertyName, Class<C> type, Method getter) {
         DomainAttribute<?> result = null;
-        if(getter.isAnnotationPresent(FloatAttribute.class)) {
+        if (getter.isAnnotationPresent(FloatAttribute.class)) {
             FloatAttribute annotation = getter.getAnnotation(FloatAttribute.class);
-            String attributeName = annotation.attributeName().isEmpty() 
-                                        ? propertyName 
-                                        : annotation.attributeName();
-            result = new FloatDomainAttribute(attributeName, annotation.maxDigitLeft(), annotation.maxDigitRight(), annotation.offset());
-        
-        } else if(getter.isAnnotationPresent(IntAttribute.class)) {
+            result = processFloatAttribute(annotation, propertyName, getter);
+
+        } else if (getter.isAnnotationPresent(IntAttribute.class)) {
             IntAttribute annotation = getter.getAnnotation(IntAttribute.class);
-            String attributeName = annotation.attributeName().isEmpty() 
-                                        ? propertyName 
-                                        : annotation.attributeName();
-            result = new IntDomainAttribute(attributeName, annotation.padding(), annotation.offset());
-        
-        } else if(getter.isAnnotationPresent(LongAttribute.class)) {
+            result = processIntAttribute(annotation, propertyName, getter);
+
+        } else if (getter.isAnnotationPresent(LongAttribute.class)) {
             LongAttribute annotation = getter.getAnnotation(LongAttribute.class);
-            String attributeName = annotation.attributeName().isEmpty() 
-                                        ? propertyName 
-                                        : annotation.attributeName();
-            result = new LongDomainAttribute(attributeName, annotation.padding(), annotation.offset());
-        
-        } else if(getter.isAnnotationPresent(SimpleDBAttribute.class)) {
-            try {
-                SimpleDBAttribute annotation = getter.getAnnotation(SimpleDBAttribute.class);
-                String attributeName = annotation.attributeName().isEmpty() 
-                                            ? propertyName 
-                                            : annotation.attributeName();
-                Class<? extends AttributeConverter<?>> converterClass = annotation.attributeConverter();
-                
-                AttributeConverter<?> converter = 
-                        (converterClass.equals(DefaultAttributeConverter.class)) 
-                            ? new DefaultAttributeConverter<C>(type)
-                            : converterClass.newInstance();
-                result = new DefaultDomainAttribute<C>(attributeName, type, (AttributeConverter<C>)converter);
-            } catch (InstantiationException ex) {
-                throw new IllegalArgumentException("Can not instanciate a converter. possible cause is that the converter class specified in @SimpleDBAttribute do not have a default constructor.", ex);
-            } catch (IllegalAccessException ex) {
-                throw new IllegalStateException("Can not instanciate a converter, because we could not be able to access the default constructor of the converter class specified in a @SimpleDBAttribute annotation.", ex);
-            }
+            result = processLongAttribute(annotation, propertyName, getter);
+
+        } else if (getter.isAnnotationPresent(SimpleDBAttribute.class)) {
+            SimpleDBAttribute annotation = getter.getAnnotation(SimpleDBAttribute.class);
+            result = processSimpleDBAttribute(annotation, propertyName, type, getter);
         } else {
             result = new DefaultDomainAttribute<C>(propertyName, type);
         }
-        
+
         return result;
     }
+
+    private DomainAttribute<Float> processFloatAttribute(FloatAttribute annotation, String propertyName, Method getter) {
+        String attributeName = annotation.attributeName().isEmpty()
+                ? propertyName
+                : annotation.attributeName();
+        return new FloatDomainAttribute(attributeName, annotation.maxDigitLeft(), annotation.maxDigitRight(), annotation.offset());
+    }
+
+    private DomainAttribute<Integer> processIntAttribute(IntAttribute annotation, String propertyName, Method getter) {
+        String attributeName = annotation.attributeName().isEmpty()
+                ? propertyName
+                : annotation.attributeName();
+        return new IntDomainAttribute(attributeName, annotation.padding(), annotation.offset());
+    }
+
+    private DomainAttribute<Long> processLongAttribute(LongAttribute annotation, String propertyName, Method getter) {
+        String attributeName = annotation.attributeName().isEmpty()
+                ? propertyName
+                : annotation.attributeName();
+        return new LongDomainAttribute(attributeName, annotation.padding(), annotation.offset());
+    }
+
+    @SuppressWarnings("unchecked")
+    private <C> DomainAttribute<C> processSimpleDBAttribute(SimpleDBAttribute annotation, String propertyName, Class<C> type, Method getter) {
+        try {
+            String attributeName = annotation.attributeName().isEmpty()
+                    ? propertyName
+                    : annotation.attributeName();
+            Class<? extends AttributeConverter<?>> converterClass = annotation.attributeConverter();
+
+            AttributeConverter<?> converter =
+                    (converterClass.equals(DefaultAttributeConverter.class))
+                    ? new DefaultAttributeConverter<C>(type)
+                    : converterClass.newInstance();
+            return new DefaultDomainAttribute<C>(attributeName, type, (AttributeConverter<C>) converter);
+        } catch (InstantiationException ex) {
+            throw new IllegalArgumentException("Can not instanciate a converter. possible cause is that the converter class specified in @SimpleDBAttribute do not have a default constructor.", ex);
+        } catch (IllegalAccessException ex) {
+            throw new IllegalStateException("Can not instanciate a converter, because we could not be able to access the default constructor of the converter class specified in a @SimpleDBAttribute annotation.", ex);
+        }
+    }
+
+    /**
+     * If an @FlatAttribute annotation is applied on a method, we must handle the class of 
+     * the return type of the method on which the annotation is applied.
+     * 
+     * @param type the return type of the method on which FlatAttribute annotation is applied.
+     */
+    private void buildFlatAttribute(Class<?> type) {
+        BeanDomainAttributes attributes = new BeanDomainAttributes(type);
+        copy(attributes, this);
+    }
     
+    private void copy(BeanDomainAttributes source, BeanDomainAttributes dest) {
+        for (String key : source.attributeMap.keySet()) {
+            if(dest.attributeMap.containsKey(key)) {
+                throw new IllegalArgumentException("Can not retrieve attributes from classes: the name of the attribute '" + key + "' of " + source.getDomainClass().getCanonicalName() + " is duplicated with the parent domainClass '" + dest.getDomainClass().getCanonicalName() + "'.");
+            }
+        }
+        dest.attributeMap.putAll(source.attributeMap);
+        dest.writeMethodMap.putAll(source.writeMethodMap);
+    }
+
     @Override
     public boolean isAttributeDefined(String attributeName) {
         return attributeMap.get(attributeName) != null;
@@ -141,10 +185,14 @@ public class BeanDomainAttributes implements DomainAttributes {
 
     @Override
     public void writeAttribute(Object instance, String attributeName, Object value) throws CanNotWriteAttributeException {
+        //TODO must process @FlatAttribute in this method.
+        
         DomainAttribute<?> attribute = attributeMap.get(attributeName);
         Method writeMethod = writeMethodMap.get(attributeName);
-        if(writeMethod == null) throw new IllegalStateException("the attribute '" + attributeName + "' is not writable.");
-        
+        if (writeMethod == null) {
+            throw new IllegalStateException("the attribute '" + attributeName + "' is not writable.");
+        }
+
         writeMethod.setAccessible(true);
         try {
             writeMethod.invoke(instance, value);
